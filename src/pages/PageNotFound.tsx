0373296aceddf6a30c9307c468a33fd6f4083ed2
Rajat Kumar Gupta <22871961+knightcube@users.noreply.github.com>
2024-02-01 10:55:03 +0530

    Enhanced 404 Error page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import MyButton from '../ui/MyButton'
+import React from "react";
+import MyButton from "../ui/MyButton";
+import Header from "../components/Header";
 
 const PageNotFound = () => {
   return (
-    <div className='flex flex-col items-center justify-center bg-black h-screen'>
-        <h1 className='text-white text-4xl'>404 | Error Not Found</h1>
-        <MyButton title="Back To Home" url='/'/>
+    <div className="h-screen">
+      <Header />
+      <div className="flex flex-col items-center justify-center h-2/3">
+        <p className="text-9xl mb-20">¯\_(ツ)_/¯</p>
+        <h1 className="text-4xl">404 | Error Not Found</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
