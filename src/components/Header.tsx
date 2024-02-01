89016b407c9c9429105015f07733f3c9a59cfbc3
Rajat Kumar Gupta <22871961+knightcube@users.noreply.github.com>
2024-02-01 10:57:20 +0530

    Fixed Broken Logo Link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 const Header: FC = () => {
   return (
     <header className="flex static bg-white lg:static z-10 items-center flex-wrap top-0 justify-between w-full mx-auto px-5 py-2 lg:px-52 lg:py-4">
-      <a href="">
+      <a href="/">
         <h1 className="text-xl font-bold">Rajat Kumar Gupta</h1>
       </a>
       {/* <div className="w-16 h-16 logo">
